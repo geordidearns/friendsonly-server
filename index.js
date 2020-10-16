223c97636ba5c9e555b1c456fdd40acf3457593f
geordidearns <geordidearns@gmail.com>
2020-10-16 22:14:37 +0200

    Add dotenv

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 const db = require("./models/index.js");
 const Vault = require("./models/vault.js");
